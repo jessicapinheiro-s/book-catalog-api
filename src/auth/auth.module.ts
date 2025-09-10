86fab089d266ee4e6dfdc3c4965526215f69ca9b
Jéssica Pinheiro <jessicasilva.js1314@gmail.com>
2025-09-10 11:53:53 -0300

    Adding prisma module to anothers one

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { JwtModule } from "@nestjs/jwt";
 import { AuthService } from "./auth.service";
 import { Module } from '@nestjs/common';
-
+import { DatabaseModule } from "../prisma/prisma.database.module";
 @Module({
     imports: [
         JwtModule.register({
@@ -9,7 +9,8 @@ import { Module } from '@nestjs/common';
             signOptions: {
                 expiresIn: '1hr'
             }
-        })
+        }),
+        DatabaseModule
     ],
     providers: [AuthService],
 })
--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -2,10 +2,12 @@ import { PrismaService } from "../prisma/prisma.service";
 import { BookService } from "./books.service";
 import { BookController } from "./books.controller";
 import { Module } from "@nestjs/common";
+import { DatabaseModule } from "../prisma/prisma.database.module";
 
 @Module({
+    imports: [DatabaseModule],
     controllers: [BookController],
     providers: [BookService, PrismaService]
 })
 
-export class BookModukle {}
+export class BookModukle { }
