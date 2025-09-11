ef47dff3fde3903509760f5f81dddc33de60e779
Jéssica Pinheiro <jessicasilva.js1314@gmail.com>
2025-09-11 10:32:39 -0300

    Inserting validators for entry data

--- src/auth/dto/auth-dto.ts
+++ src/auth/dto/auth-dto.ts
@@ -1,15 +1,28 @@
+import { IsEmail, IsNotEmpty, MinLength } from "class-validator";
+
 export class CreateUserDto {
+    @IsEmail()
+    @IsNotEmpty()
     email: string;
+    @IsNotEmpty()
     name: string;
+    @IsNotEmpty()
+    @MinLength(8)
     password: string;
 }
 
 export class LoginUserDto {
+    @IsEmail()
+    @IsNotEmpty()
     email: string;
+    @IsNotEmpty()
+    @MinLength(8)
     password: string;
 }
 
 export class ResetPasswordDto {
     token: string;
+    @IsNotEmpty()
+    @MinLength(8)
     newPassword: string;
 }
--- src/books/dto/book.tdo.ts
+++ src/books/dto/book.tdo.ts
@@ -1,24 +1,41 @@
+import { IsNotEmpty, MinLength } from "class-validator";
+
 export class User {
     id: number;
+    @IsNotEmpty()
     name: string;
+    @IsNotEmpty()
     email: string;
+    @MinLength(8)
+    @IsNotEmpty()
     password: string;
     books: Book[]
 }
 
 export class Book {
     id?: number;
+    @IsNotEmpty()
     title: string;
+    @IsNotEmpty()
     author: string;
+    @IsNotEmpty()
     genre: string;
+    @IsNotEmpty()
     status: string;
+    @IsNotEmpty()
     userId: number;
+    @IsNotEmpty()
     user?: User;
 }
 export class BookUpdate {
+    @IsNotEmpty()
     id?: number;
+    @IsNotEmpty()
     title?: string;
+    @IsNotEmpty()
     author?: string;
+    @IsNotEmpty()
     genre?: string;
+    @IsNotEmpty()
     status?: string;
 }
